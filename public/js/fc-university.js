b9873fb2668e71a8af76c015a393856126c4e43d
Kamleshwar <kamleshwar.maurya@publicissapient.com>
2019-10-10 01:53:16 +0530

    added html for fc-university demo page

---
+++ public/js/fc-university.js
@@ -0,0 +1,36 @@
+
+
+
+$(document).ready(function () {
+    //initialize swiper when document ready
+    var heroCarousel = new Swiper('.swiper-container', {
+        autoplay: {
+            delay: 1000,
+        },
+    })
+
+
+    var testimonyCarousel = new Swiper('.testimony-container', {
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    })
+    var teamCarousel = new Swiper('.team-container', {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    })
+});
+
